f0dd9a3e19b9f578dbfd80e149b205fba907c9ce
Austin Koenig <austindkoenig@gmail.com>
2020-10-11 02:28:38 -0700

    Player animations all have time to play before going to the next animation.

--- Project/Scripts/Player.gd
+++ Project/Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody
 
-const GRAVITY = 500
+const GRAVITY = 300
 const BASE_SPEED = 20
 const DASH_SPEED = 200
 const ACCELERATION = 100
@@ -13,10 +13,16 @@ onready var animation_state = $PlagueDoctaModel/AnimationTree.get("parameters/pl
 var direction = Vector3.ZERO
 var velocity = Vector3.ZERO
 var h_rotation = 0
+
+var jumping = false
 var jump = 0
 
-var dash = false
+var dashing = false
 var dash_enabled = true
+
+var poking = false
+
+var throwing = false
 
 
 # PRIVATE METHODS
@@ -30,10 +36,11 @@ func _input(event: InputEvent) -> void:
 			get_tree().quit()
 
 func _process(delta: float) -> void:
-	if direction != Vector3.ZERO:
-		animation_state.travel("Walk")
-	else:
-		animation_state.travel("Idle")
+	if not (jumping or dashing or poking or throwing):
+		if direction != Vector3.ZERO:
+			animation_state.travel("Walk")
+		else:
+			animation_state.travel("Idle")
 
 func _physics_process(delta: float) -> void:
 	# Update direction
@@ -48,20 +55,20 @@ func _physics_process(delta: float) -> void:
 		$PlagueDoctaModel.rotation.y = lerp_angle($PlagueDoctaModel.rotation.y, $CameraRoot/H.rotation.y, delta * ANGULAR_ACCELERATION)
 	
 	# Jump
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and (not jumping):
 		jump = JUMP_POWER
+		jumping = true
 		animation_state.travel("Jump")
+		$JumpAnimationTimer.start()
 	else:
 		jump = 0
 	
-	velocity.y += jump
-	
 	# Dash
 	if Input.is_action_just_pressed("dash") and dash_enabled:
-		dash = true
+		dashing = true
 		dash_enabled = false
 		animation_state.travel("Dash")
-		$DashTimer.start()
+		$DashAnimationTimer.start()
 		$DashCooldownTimer.start()
 	
 	# Gravity
@@ -72,9 +79,11 @@ func _physics_process(delta: float) -> void:
 	
 	# Movement
 	if direction != Vector3.ZERO:
-		velocity = velocity.move_toward(direction * (BASE_SPEED + DASH_SPEED * int(dash)), delta * ACCELERATION)
+		velocity = velocity.move_toward(direction * (BASE_SPEED + DASH_SPEED * int(dashing)), delta * ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector3.ZERO, delta * ACCELERATION)
+	
+	velocity.y += jump * int(jumping)
 	
 	move_and_slide(velocity, Vector3(0, 1, 0))
 
@@ -86,8 +95,22 @@ func change_health(change: int) -> void:
 
 # SIGNALS
 
-func _on_DashTimer_timeout() -> void:
-	dash = false
-
 func _on_DashCooldownTimer_timeout() -> void:
 	dash_enabled = true
+
+func _on_DashAnimationTimer_timeout() -> void:
+	dashing = false
+	animation_state.travel("Idle")
+
+func _on_JumpAnimationTimer_timeout() -> void:
+	jumping = false
+	jump = 0
+	animation_state.travel("Idle")
+
+func _on_PokeAnimationTimer_timeout() -> void:
+	poking = false
+	animation_state.travel("Idle")
+
+func _on_ThrowAnimationTimer_timeout() -> void:
+	throwing = false
+	animation_state.travel("Idle")
